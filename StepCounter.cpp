1f40bd19221839f2927a08bf4a66f4c9cbce7bb6
Dhruv-Mahajan <dhruv.mahajan001@gmail.com>
2023-05-04 20:07:05 +0530

    Add Step counter

---
+++ StepCounter.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+
+int steps;
+int lastReading;
+int threshold = 20;
+int latency = 100; 
+// Simulate sensor readings
+int random(int min, int max) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_int_distribution<> distrib(min, max);
+    return distrib(gen);
+}
+
+int readSensor()
+{
+    int randInt = random(1, 100);
+    return randInt;
+}
+
+int countSteps(int reset) {
+    if(reset==0)
+    {
+        steps = 0;
+        lastReading = 0; 
+    }
+    int reading = readSensor();
+    this_thread::sleep_for(std::chrono::milliseconds(latency));
+    if (reading > lastReading + threshold)steps++;
+    lastReading = reading;
+    return steps;
+}
+
+int main() {
+    cout<<countSteps(0)<<'\n';
+    for (int i=0;i<10;i++)
+    {
+        cout<<countSteps(1)<<'\n';
+        this_thread::sleep_for(std::chrono::seconds(2));
+    }
+
+
+    return 0;
+}
